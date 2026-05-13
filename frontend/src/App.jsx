fd8251148fddaf515b2e61148b87bbf9a1cd1afe
samuelnchege <nchegesamuel@gmail.com>
2026-05-12 20:18:28 -0800

    frontend setup with React, Vite and Tailwind v4

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-600">
+        Croil Market is Live!
+      </h1>
+    </div>
+  )
+}
+
+export default App
